86e4c5d1fa7dae0b76b5ce4e8cc87d712883d35a
mmA1r <mask.matveev@mail.ru>
2022-11-28 04:19:23 +0400

    fix some issues

--- src/pages/gamePage/game/source/getCastle/getCastle.js
+++ src/pages/gamePage/game/source/getCastle/getCastle.js
@@ -6,15 +6,15 @@ export default async function getCastle(scene) {
     const server = store.getState().server.value;
     let data = await server.getCastle();
     if (data) {
-        scene.store.loadToStore({money:data.money-0},'gamer');
-        scene.store.loadToStore({level:data.level-0},'gamer');
+        scene.store.loadToStore({ money: data.money-0 },'gamer');
+        scene.store.loadToStore({ level: data.level-0 },'gamer');
         scene.player = data.id;
         scene.myCastle = {
             x: Math.round(data.posX * 64),
             y: Math.round(data.posY * 64),
             level: data.level
         };
-        scene.cameras.main.centerOn(scene.myCastle.x,scene.myCastle.y);
+        scene.cameras.main.centerOn(scene.myCastle.x, scene.myCastle.y);
         return data;
     }
 }
