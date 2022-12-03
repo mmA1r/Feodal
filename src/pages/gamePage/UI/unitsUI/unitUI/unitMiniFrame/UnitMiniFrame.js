24e045b214c511827ff45dab424aed8aff1058bc
mmA1r <mask.matveev@mail.ru>
2022-12-04 01:16:56 +0400

    unit panel

--- src/pages/gamePage/UI/unitsUI/unitUI/unitMiniFrame/UnitMiniFrame.js
+++ src/pages/gamePage/UI/unitsUI/unitUI/unitMiniFrame/UnitMiniFrame.js
@@ -1,11 +1,12 @@
+import { useSelector } from 'react-redux';
 import './unitMiniFrame.scss';
 
 export default function UnitMiniFrame() {
+
+    const unitLevel = useSelector((state) => state.currentUnit.level);
+
     return (
-        <div className='unit-frame-panel'>
-            <svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="75" cy="75" r="71.5" fill="black" stroke="#53291C" strokeWidth="7"/>
-            </svg>
+        <div className={`unit-frame-panel${unitLevel}`}>
         </div>
     );
 }
--- src/store/features/currentUnit/currentUnit.js
+++ src/store/features/currentUnit/currentUnit.js
@@ -4,27 +4,19 @@ export const unit = createSlice({
     name: 'unit',
     initialState: {
         hp: 0,
-        damage: 0,
-        speed: 0,
-        might: 0,
-        type: 1
+        type: 1,
+        level: 1
     },
     reducers: {
         currentUnit: (state, action) => {
             if('hp' in action.payload) {
                 state.hp = action.payload.hp;
             }
-            if('damage' in action.payload) {
-                state.damage = action.payload.damage;
-            }
-            if('speed' in action.payload) {
-                state.speed = action.payload.speed;
-            }
-            if('might' in action.payload) {
-                state.might = action.payload.might;
-            }
             if('type' in action.payload) {
                 state.type = action.payload.type;
+            }
+            if('level' in action.payload) {
+                state.level = action.payload.level;
             }
         }
     }
