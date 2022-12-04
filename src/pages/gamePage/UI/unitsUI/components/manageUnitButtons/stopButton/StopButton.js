46fa5f42d11b5dd897160c21a55c3e180f720155
mmA1r <mask.matveev@mail.ru>
2022-12-04 05:07:59 +0400

    ...

--- src/pages/gamePage/UI/unitsUI/components/manageUnitButtons/stopButton/StopButton.js
+++ src/pages/gamePage/UI/unitsUI/components/manageUnitButtons/stopButton/StopButton.js
@@ -7,7 +7,7 @@ export default function StopButton() {
 
     return (
         <svg className={`${stop ? 'stop-active' : 'stop-inactive' }`} width="155" height="21" viewBox="0 0 155 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path className='stop-button-background' d="M145.237 7.53524L145.129 7.56479L145.029 7.61765L137.529 11.6176L137.248 11.7677L137.106 12.0528L135.106 16.0528L135 16.2639V16.5V20H20.8022L19.9762 16.2831L19.9495 16.1629L19.8944 16.0528L17.8944 12.0528L17.7519 11.7677L17.4706 11.6176L9.97059 7.61765L9.87148 7.56479L9.76312 7.53524L4.69447 6.15288L2.11803 1H152.882L150.306 6.15288L145.237 7.53524Z" fill="#411F1B" stroke="#AAAAAA" stroke-width="2"/>
+            <path className='stop-button-background' d="M145.237 7.53524L145.129 7.56479L145.029 7.61765L137.529 11.6176L137.248 11.7677L137.106 12.0528L135.106 16.0528L135 16.2639V16.5V20H20.8022L19.9762 16.2831L19.9495 16.1629L19.8944 16.0528L17.8944 12.0528L17.7519 11.7677L17.4706 11.6176L9.97059 7.61765L9.87148 7.56479L9.76312 7.53524L4.69447 6.15288L2.11803 1H152.882L150.306 6.15288L145.237 7.53524Z" fill="#411F1B" stroke="#AAAAAA" strokeWidth="2"/>
             <path className='stop-button-shadow' d="M76.5 13.8044L84.0307 6.27371L84.7378 6.98082L77.2071 14.5115L76.5 13.8044Z" fill="black" fillOpacity="0.3"/>
             <path className='stop-button-shadow' d="M86 10.5C86 13.5376 83.5376 16 80.5 16C77.4624 16 75 13.5376 75 10.5C75 7.46243 77.4624 5 80.5 5C83.5376 5 86 7.46243 86 10.5Z" stroke="black" strokeOpacity="0.3"/>
             <rect className='stop-button-icon' x="74.0219" y="13.771" width="10.65" height="1" transform="rotate(-45 74.0219 13.771)" fill="#AAAAAA"/>
--- src/pages/gamePage/UI/unitsUI/unitUI/unitMiniFrame/UnitMiniFrame.js
+++ src/pages/gamePage/UI/unitsUI/unitUI/unitMiniFrame/UnitMiniFrame.js
@@ -3,10 +3,11 @@ import './unitMiniFrame.scss';
 
 export default function UnitMiniFrame() {
 
+    const unitType = useSelector((state) => state.currentUnit.type);
     const unitLevel = useSelector((state) => state.currentUnit.level);
 
     return (
-        <div className={`unit-frame-panel${unitLevel}`}>
+        <div className={`unit-frame-panel${unitLevel}-${unitType}`}>
         </div>
     );
 }
