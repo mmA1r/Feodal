a5e42014c1acf1e4f8fb33e5db8264fdbd389ff1
astar1ka <astar1ka@mail.ru>
2023-01-26 17:27:09 +0400

    Update Unit.js

--- src/pages/gamePage/game/source/entites/Unit.js
+++ src/pages/gamePage/game/source/entites/Unit.js
@@ -258,6 +258,7 @@ export default class Unit extends Entity {
 
     killed() {
         this.stopped();
+        this.unselect();
         if (this.scene.player.selectedObject) this.scene.player.updateUI();
         this.body.enable = false;
         this.pointer.destroy();
